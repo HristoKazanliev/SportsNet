57e6c7f113e594616e7b64603c7a657e15b3ce2b
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2023-07-22 15:12:36 +0300

    Add project files.

---
+++ SportsNet.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SportsNet.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
